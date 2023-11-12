07a2ad81f18155ce1353cc0068f295ffbd2dbbf4
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-11-11 19:10:43 -0700

    Update code.js
    update code.js

--- code.js
+++ code.js
@@ -1,3 +1,30 @@
 function allPairsShortestPaths(graph) {
-    return [];
+    
+    var V = graph.length;
+    var dist = [];
+    var i,j,k;
+    //create dist
+    for(i = 0; i < V; i++) {
+        dist[i] = [];
+        for(k = 0; k < V; k++) {
+            dist[i][k] = 0;
+        }
+    }
+    //copy graph to dist
+    for(i = 0; i < V; i++) {
+        for(k = 0; k < V; k++) {
+            dist[i][k] = graph[i][k];
+        }
+    }
+    for(k = 0; k < V; k++) {
+        for(i = 0; i <V; i++) {
+            for(j = 0; j <V; j++) {
+                if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+    return dist;
 }
+
